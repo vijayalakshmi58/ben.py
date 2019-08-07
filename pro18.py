ca3c8959fef38d80801d3e3688a85e80a5d59ec5
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-08-07 15:29:09 +0530

    Update pro18.py

--- pro18.py
+++ pro18.py
@@ -1,16 +1,16 @@
-viji,sun=map(int,input().split())
-sang=[]
-tell=0
+viji,sun1=map(int,input().split())
+sang1=[]
+tel1=0
 for i in range(viji):
-    sang.append(list(map(int,input().split())))   
+    sang1.append(list(map(int,input().split())))   
 for i in range(viji):
-    for j in range(sun-1):
-        for k in range(j+1,sun+1):
-            if sang[i][j:k]==[1]*len(sang[i][j:k]):
-                 if all(sang[p+i][j:k]==[1]*len(sang[p+i][j:k]) for p in range(len(sang[i][j:k])-1)):
-                     if len(sang[i][j:k])>tel:
-                        tell=len(sang[i][j:k])
-if len(sang)==1 and len(sang[0])==1 and sang[0][0]==1:
+    for j in range(sun1-1):
+        for k in range(j+1,sun1+1):
+            if sang1[i][j:k]==[1]*len(sang1[i][j:k]):
+                 if all(sang1[p+i][j:k]==[1]*len(sang1[p+i][j:k]) for p in range(len(sang1[i][j:k])-1)):
+                     if len(sang1[i][j:k])>tel1:
+                        tel1=len(sang1[i][j:k])
+if len(sang1)==1 and len(sang1[0])==1 and sang1[0][0]==1:
     print(1)
-for i in range(tel):
-    print(*[1]*tell)
+for i in range(tel1):
+    print(*[1]*tel1)
