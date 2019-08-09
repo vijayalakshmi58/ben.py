f3cc26ed08fe4ab094da12d2eaa9d56efd78091f
T.vijayalakshmi <52314223+vijayalakshmi58@users.noreply.github.com>
2019-08-09 01:52:21 -0700

    Update pro27.py

--- pro27.py
+++ pro27.py
@@ -1,16 +1,16 @@
-viji,ss=map(int,input().split())
+viji1,jj1=map(int,input().split())
 ts=list(map(int,input().split()))
 tsp=list(map(int,input().split()))
 thn=[]
 cin=0
-for i in range(viji):
+for i in range(viji1):
     x=tsp[i]/ts[i]
     thn.append(x)
-while jj>=0 and len(thn)>0:
+while jj1>=0 and len(thn)>0:
     mindex=thn.index(max(thn))
-    if jj>=ts[mindex]:
+    if jj1>=ts[mindex]:
         cin=cin+tsp[mindex]
-        jj=jj-ts[mindex]
+        jj1=jj1-ts[mindex]
     ts.pop(mindex)
     tsp.pop(mindex)
     thn.pop(mindex)
